e3b293eb4625887c5b2f257295713552a633f851
Vtigami <136007717+Vtigami@users.noreply.github.com>
2023-06-28 01:10:15 -0300

    Update serFunc.c

--- serFunc.c
+++ serFunc.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include"commonFunc.h"
 #include"serFunc.h"
+#include"cliFunc.h"
+#include  <openssl/md5.h>
 
 void receiveBackup(int s,mensagem_t* m,unsigned char* seq,char* current_path){
 
     char path[MAXPATH];
     strcpy(path, current_path);
 
-    if(strcmp(current_path,"./")!=0)
-        strcat (path, "/");
+    strcat (path, "/");
     strcat (path, (char*)m->dados);
 
     FILE *arq = openFile(path,"wb");
     if(!arq){
         sendEmpty(s,*seq,NACK);
-        //addToSeq(seq,-1);
         return;
     }
 
@@ -36,10 +37,8 @@ void receiveBackup(int s,mensagem_t* m,unsigned char* seq,char* current_path){
 
         if(verifyMsg(buffer,size)){
             separateMessage(&m2, buffer);
-            if(m2.tipo!=ACK&&m2.tipo!=NACK){
-            if(*seq==m2.sequencia){
-                printf("mensagem %d recebida! tipo(%d) tam(%d)\n",*seq,m2.tipo,m2.tamanho);
-                //addToSeq(seq,1);
+            if(*seq==m2.sequencia&&m2.tipo!=ACK&&m2.tipo!=NACK){
+                
                 switch(m2.tipo){
 
                     case DATA:
@@ -48,7 +47,6 @@ void receiveBackup(int s,mensagem_t* m,unsigned char* seq,char* current_path){
                             addToSeq(seq,1);
                         }else{
                             sendEmpty(s,*seq,NACK);
-                            //addToSeq(seq,1);
                         }
                     break;
 
@@ -57,11 +55,6 @@ void receiveBackup(int s,mensagem_t* m,unsigned char* seq,char* current_path){
                         sendEmpty(s,*seq,ACK);
                         endof=1;
                         addToSeq(seq,1);
-                    break;
-                    default:
-                        printf("why????\n");
-                        sendEmpty(s,*seq,NACK);
-                        //addToSeq(seq,1);
                     break;
 
                 }
@@ -72,12 +65,53 @@ void receiveBackup(int s,mensagem_t* m,unsigned char* seq,char* current_path){
                 else
                     sendEmpty(s,getSeqAdding(seq,-1),ACK);
             }   
-            }               
+                         
 
         }
     }while(!endof); 
-    printf("saiiiuuuuuu!\n");
+    printf("Arquivo %s foi transmitido com sucesso.\n",(char*)m->dados);
     fclose(arq);
+
+}
+
+void verifyFileServer(int s,char* filename,char* current_path,unsigned char* seq){
+
+    char path[MAXPATH];
+    unsigned char buffer[MAXBUFF];
+    mensagem_t m2;
+    int size;
+    
+
+    if(fileExists(filename, current_path)){
+
+        strcpy(path, current_path);
+        strcat (path, "/");
+        strcat (path, filename);
+        // mensagem de "arquivo existe"
+        FILE *fileServer = openFile(path,"rb");
+        if(!fileServer){
+            sendEmpty(s,*seq,ERROR);
+            addToSeq(seq,1);
+            return;
+        }
+
+        unsigned char *result = getMD5(fileServer); // Gera o MD5 Do arquivo do servidor
+
+        setMsgAttr(&m2,MD5_DIGEST_LENGTH,*seq, MMD5);
+        memcpy((char*)m2.dados, result,MD5_DIGEST_LENGTH);
+        size=fillBuffer(&m2,buffer);
+        if(send(s, buffer,size, 0)<0)
+             perror("erro no envio da mensagem:");
+        addToSeq(seq,1);
+
+        free(result);
+        fclose(fileServer);
+    }else{
+        sendEmpty(s,*seq,ERROR);
+        addToSeq(seq,1);
+        printf("Arquivo nao existe\n");
+        return;
+    }
 
 
 }
