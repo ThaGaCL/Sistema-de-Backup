1d6b32c8d3ea7dbc98ade606e4ed5353cebbccff
Vtigami <136007717+Vtigami@users.noreply.github.com>
2023-06-28 01:07:57 -0300

    Update cliFunc.h

--- cliFunc.h
+++ cliFunc.h
@@ -1,16 +1,21 @@
 #define MAXLINE 1024
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_CYAN    "\x1b[96m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define CLIPATH "./clientfolder"
 
 void menu();
 
 void custom_ls(DIR *dirstream);
 
-void custom_cd(char* path,char* current_path);
+void custom_cd(char* path,char* current_path,char* base_path);
 
 void backup(int s,char* filename,char* current_path,unsigned char *seq);
 
 void firstWord(char** word,char* s);
 
 void replaceFirst(char* s,char c1, char c2);
+
+void setSerDir(int s,char* path,unsigned char* seq);
+
+void requestBackup(int s,char* filename,unsigned char* seq);
+
+void verifyFile(int s,char* filename,unsigned char* seq,char* current_path);
