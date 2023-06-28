fea3762d592b34ad77aed6a8074c84ef35aab76d
Vtigami <136007717+Vtigami@users.noreply.github.com>
2023-06-28 01:10:47 -0300

    Update serFunc.h

--- serFunc.h
+++ serFunc.h
@@ -1,3 +1,4 @@
-
+#define SERPATH "./serverfolder"
 
 void receiveBackup(int s,mensagem_t* m,unsigned char* seq,char* current_path);
+void verifyFileServer(int s,char* filename,char* current_path,unsigned char* seq);
