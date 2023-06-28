6db5894321a196d2236a9261d270ba4303bd35a9
Vtigami <136007717+Vtigami@users.noreply.github.com>
2023-06-28 01:08:28 -0300

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -11,6 +11,7 @@
 #include <dirent.h>
 #include"commonFunc.h"
 #include"cliFunc.h"
+#include"serFunc.h"
 
 
 
@@ -18,14 +19,15 @@
 int main(){
 
     int s = create_socket("lo");//eno1
-    char current_path[MAXPATH]="./";
+    char current_path[MAXPATH]=CLIPATH;
     DIR *dirstream = openDir(current_path);
     if(!dirstream){
         perror("Erro na abertura do diretorio");
         exit(1);
 
     }
-        
+     
+    mensagem_t m;   
     char entry[MAXLINE];
     char* value;
     int ended=0;
@@ -41,7 +43,7 @@ int main(){
             custom_ls(dirstream);
 
         }else if(strcmp(value,"cd")==0){
-            custom_cd(entry,current_path);
+            custom_cd(entry,current_path,CLIPATH);
             dirstream = openDir(current_path);
 
         }else if(strcmp(value,"backup")==0){
@@ -51,11 +53,25 @@ int main(){
             }
 
         }else if(strcmp(value,"recover")==0){
+            while(strcmp(entry,"\0")!=0){
+                firstWord(&value,entry);
+                requestBackup(s,value,&seq);
+                strcpy((char*)m.dados,value);
+                receiveBackup(s,&m,&seq,current_path);
+                do{
 
+                    sendEmpty(s,getSeqAdding(&seq,-1),ACK);
+
+                }while(recvMensagem(s,END,&seq)<0);
+                
+            }
         }else if(strcmp(value,"defdir")==0){
-
+            setSerDir(s,entry,&seq);
         }else if(strcmp(value,"verify")==0){
-
+            if(strcmp(entry,"\0")!=0){
+                firstWord(&value,entry);
+                verifyFile(s,value,&seq,current_path);
+            }
         }else if(strcmp(value,"exit")==0||strcmp(value,"q")==0){
             ended=1;
 
